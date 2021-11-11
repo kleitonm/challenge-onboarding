bed63e45e84a2704b0d8b756988f547cb14ac1ef
kleitonm <kleiton_mendes@hotmail.com>
2021-11-11 20:58:52 -0300

    Implementação da EmptyView

---
+++ solutions/devsprint-6/OnboardingChallenge/Components/ViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ViewController.swift
+//  OnboardingChallenge
+//
+//  Created by Kleiton Mendes on 10/11/2021.
+//  Copyright © 2021 Rodrigo Borges. All rights reserved.
+//
+
+import Foundation
